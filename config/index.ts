24a8150a2b9416483be5ad3c19910a80c343a10a
janily <nanhuchenjie@163.com>
2023-10-28 15:53:23 +0800

    初始化文件

--- config/index.ts
+++ config/index.ts
@@ -1,10 +1,10 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const APP_ID = `b5ea2c0d-8d35-4316-81a3-9a0ba9c0669e`
+export const API_KEY = `app-9jCsYESQxiBEawIzYAEYcixr`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: 'Chat-Threejs',
+  description: 'Chat with threejs,react three fiber, theatre.js',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',
@@ -18,3 +18,4 @@ export const API_PREFIX = '/api'
 export const LOCALE_COOKIE_NAME = 'locale'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
+
